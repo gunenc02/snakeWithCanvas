33f6f20978511d8bc63ac6a835fe17ae02b0d23d
Bahadır Günenc <bahadir.gunenc@ug.bilkent.edu.tr>
2022-06-23 15:57:00 +0300

    max score, score, hidden game over, snakeEntry last pixel problem need to be handled

--- app.js
+++ app.js
@@ -7,16 +7,27 @@ let startY = 50;
 let endX = 200;
 let endY = 50;
 
-let canvasHeight = 300;
-let canvasWidth = 1200;
-
-let snake = [[20, 20, 20, 30]]
+let canvasHeight = 450;
+let canvasWidth = 800;
+var snakeArray = [
+    [20, 20, 20, 30],
+    [20, 30, 20, 40],
+    [20, 40, 20, 50]
+];
+let timer;
 let directionNumber = 0;
+
+let score = 0;
+let gameSpeed = 400;
 
 body.addEventListener('keydown', function (e) {
 
-    if (e.key === "ArrowUp" && (directionNumber !== 1 && directionNumber !== 2)) {
-        if(directionNumber === 0){
+
+    if (directionNumber !== 0 && directionNumber !== 1 && directionNumber !== 2 && directionNumber !== 3 && directionNumber !== 4) {
+        return
+    }
+    else if (e.key === "ArrowUp" && (directionNumber !== 1 && directionNumber !== 2)) {
+        if (directionNumber === 0) {
             directionNumber = 1;
             startGame();
         }
@@ -25,7 +36,7 @@ body.addEventListener('keydown', function (e) {
     }
 
     else if (e.key === "ArrowDown" && (directionNumber !== 1 && directionNumber !== 2)) {
-        if(directionNumber === 0){
+        if (directionNumber === 0) {
             directionNumber = 2;
             startGame();
         }
@@ -34,7 +45,7 @@ body.addEventListener('keydown', function (e) {
     }
 
     else if (e.key === "ArrowLeft" && (directionNumber !== 3 && directionNumber !== 4)) {
-        if(directionNumber === 0){
+        if (directionNumber === 0) {
             directionNumber = 3;
             startGame();
         }
@@ -42,38 +53,37 @@ body.addEventListener('keydown', function (e) {
         directionNumber = 3;
     }
 
-    else if (e.key === "ArrowRight" && (directionNumber !== 3 && directionNumber !== 4)) {  
-        if(directionNumber === 0){
+    else if (e.key === "ArrowRight" && (directionNumber !== 3 && directionNumber !== 4)) {
+        if (directionNumber === 0) {
             directionNumber = 4;
             startGame();
         }
 
-        directionNumber = 4;            
+        directionNumber = 4;
     }
-    decidingCreation();
 
 });
 
-function decidingCreation(){
-    console.log(snake[0][2]);
-    let firstX = snake[0][2];
-    let firstY = snake[0][3];
-    let lastX = snake[0][2];
-    let lastY = snake[0][3];
+function decidingCreation() {
 
-    if(directionNumber === 1) {
-        lastY -= 10; 
+    let firstX = snakeArray[0][2];
+    let firstY = snakeArray[0][3];
+    let lastX = snakeArray[0][2];
+    let lastY = snakeArray[0][3];
+
+    if (directionNumber === 1) {
+        lastY -= 10;
     }
 
-    else if(directionNumber === 2) {
+    else if (directionNumber === 2) {
         lastY += 10;
     }
 
-    else if( directionNumber === 3) {
+    else if (directionNumber === 3) {
         lastX -= 10;
     }
 
-    else {
+    else if (directionNumber === 4) {
         lastX += 10;
     }
     newSegment(firstX, firstY, lastX, lastY);
@@ -82,50 +92,86 @@ function decidingCreation(){
 
 function snakeDrawer() {
 
-    ctx.clearRect(0, 0, 1200, 300);
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     if (isAppleEaten()) {
         randomAppleCreator();
     } else {
         appleCreator();
     }
 
-    snake.pop();
+    snakeArray.pop();
 
-    for (let i = 0; i < snake.length; i++) {
-        console.log("entered");
-        ctx.lineJoin = "round";
-        ctx.beginPath();
-        ctx.moveTo(snake[i][0], snake[i][1]);
-        ctx.lineTo(snake[i][2], snake[i][3]);
-        ctx.lineWidth = 10;
-        ctx.stroke();
+    let prevX = snakeArray[0][0];
+    let prevY = snakeArray[0][1];
+    ctx.lineJoin = "round";
+    ctx.fillStyle = "gray"
+    ctx.beginPath();
+    ctx.moveTo(snakeArray[0][2], snakeArray[0][3]);
+    ctx.lineWidth = 10;
+
+    for (let i = 0; i < snakeArray.length; i++) {
+
+        if (Math.abs(prevX - snakeArray[i][0]) < 11 && Math.abs(prevY - snakeArray[i][1]) < 11) {
+            ctx.lineTo(snakeArray[i][0], snakeArray[i][1]);
+            prevX = snakeArray[i][0];
+            prevY = snakeArray[i][1];
+        } else {
+            ctx.stroke();
+            ctx.lineJoin = "round";
+            ctx.beginPath();
+            ctx.moveTo(snakeArray[i][2], snakeArray[i][3]);
+            ctx.lineWidth = 10;
+            prevX = snakeArray[i][0];
+            prevY = snakeArray[i][1];
+        }
+
     }
+    ctx.stroke();
+    isGameFinished();
 }
 
 function randomAppleCreator() {
-   
-    centerX = Math.ceil(Math.random() * 1280 + 10);
-    centerY = Math.ceil(Math.random() * 280 + 10);
+
+    centerX = Math.ceil(Math.random() * canvasWidth-10 + 5);
+    centerY = Math.ceil(Math.random() * canvasHeight-10 + 5);
+    if(snakeLocation(centerX, centerY)){
+        randomAppleCreator();
+    }
 
     appleCreator();
-}    
-    
+}
+
 
 function canvasPosition() {
 
     canvas.setAttribute("height", `${canvasHeight}`);
     canvas.setAttribute("width", `${canvasWidth}`);
     canvas.setAttribute("style", "border:1px dotted black");
-    canvas.style.top = "200px";
-    canvas.style.left = "50px";
-    canvas.style.position = "relative";
+    canvas.style.top = "20%";
+    canvas.style.left = "30%";
+    canvas.style.position = "absolute";
+    canvas.style.backgroundColor = "green"
 
     ctx.lineJoin = "round";
     ctx.beginPath();
-    ctx.moveTo(snake[0][0], snake[0][1]);
-    ctx.lineTo(snake[0][2], snake[0][3]);
+    ctx.color = "red"
+    ctx.moveTo(snakeArray[0][0], snakeArray[0][1]);
+    ctx.lineTo(snakeArray[0][2], snakeArray[0][3]);
+    ctx.lineTo(snakeArray[1][2], snakeArray[1][3]);
+    ctx.lineTo(snakeArray[2][2], snakeArray[2][3]);
     ctx.lineWidth = 10;
     ctx.stroke();
+
+    ctx.font = "20px sanserif";
+    ctx.textAlign = "left";
+    ctx.fillStyle = "brown"
+    ctx.fillText("Score: " + 20, 50);
+
+    ctx.font = "20px sanserif";
+    ctx.textAlign = "right";
+    ctx.fillStyle = "brown";
+    ctx.fillText(`Max Score: ${maxEatenApple}` + 20, 50);
+    
 }
 
 function appleCreator() {
@@ -137,27 +183,31 @@ function appleCreator() {
 }
 
 function newSegment(firstX, firstY, lastX, lastY) {
-    if(lastX < 0 || lastX > 1200 || lastY < 0 || lastY > 300 ){    
-        positionFormatter();
-    }
+
     let newSegment = [0, 0, 0, 0];
     newSegment[0] = firstX;
     newSegment[1] = firstY;
     newSegment[2] = lastX;
     newSegment[3] = lastY;
 
-    snake.unshift(newSegment);
-    snakeDrawer();
+    snakeArray.unshift(newSegment);
+
+    if (lastX < 0 || lastX > canvasWidth || lastY < 0 || lastY > canvasHeight) {
+        positionFormatter();
+    }
+
+
+
 }
 
 function startGame() {
 
-    setInterval(decidingCreation, 200);
+    timer = setInterval(decidingCreation, gameSpeed);
 }
 
 function isAppleEaten() {
-    console.log("ım working isAppleEaten")
-    if (Math.abs(snake[0][2] - centerX) < 5 && Math.abs(snake[0][3] - centerY) < 5) {
+
+    if (Math.abs(snakeArray[0][2] - centerX) <= 8 && Math.abs(snakeArray[0][3] - centerY) <= 8) {
         grow();
         return true;
     }
@@ -166,71 +216,101 @@ function isAppleEaten() {
 }
 
 function grow() {
-    console.log("ım working grow")
-    if (snake[snake.length - 1][0] - snake[snake.length - 1][2] == 0 &&
-         snake[snake.length - 1][3] - snake[snake.length - 1][1] < 0) {
 
-        let newSnakeSegment = [snake[snake.length - 1][0], snake[snake.length-1][1] + 20,
-        snake[snake.length-1][2], snake[snake.length-1][3]] ;
-        snake.push(newSnakeSegment);
+    if (snakeArray[snakeArray.length - 1][0] - snakeArray[snakeArray.length - 1][2] == 0 &&
+        snakeArray[snakeArray.length - 1][3] - snakeArray[snakeArray.length - 1][1] < 0) {
+
+        let newSnakeSegment = [snakeArray[snakeArray.length - 1][0], snakeArray[snakeArray.length - 1][1] + 20,
+        snakeArray[snakeArray.length - 1][2], snakeArray[snakeArray.length - 1][3]];
+        snakeArray.push(newSnakeSegment);
 
     }
 
-    else if (snake[snake.length - 1][0] - snake[snake.length - 1][2] == 0 &&
-         snake[snake.length - 1][3] - snake[snake.length - 1][1] > 0) {
+    else if (snakeArray[snakeArray.length - 1][0] - snakeArray[snakeArray.length - 1][2] == 0 &&
+        snakeArray[snakeArray.length - 1][3] - snakeArray[snakeArray.length - 1][1] > 0) {
 
-        let newSnakeSegment = [snake[snake.length - 1][0], snake[snake.length-1][1] - 20,
-        snake[snake.length-1][2], snake[snake.length-1][3]] ;
-        snake.push(newSnakeSegment);
+        let newSnakeSegment = [snakeArray[snakeArray.length - 1][0], snakeArray[snakeArray.length - 1][1] - 20,
+        snakeArray[snakeArray.length - 1][2], snakeArray[snakeArray.length - 1][3]];
+        snakeArray.push(newSnakeSegment);
     }
 
-    else if (snake[snake.length - 1][0] - snake[snake.length - 1][2] < 0 &&
-         snake[snake.length - 1][3] - snake[snake.length - 1][1] == 0) {
+    else if (snakeArray[snakeArray.length - 1][0] - snakeArray[snakeArray.length - 1][2] < 0 &&
+        snakeArray[snakeArray.length - 1][3] - snakeArray[snakeArray.length - 1][1] == 0) {
 
-        let newSnakeSegment = [snake[snake.length - 1][0] - 20, snake[snake.length-1][1],
-        snake[snake.length-1][2], snake[snake.length-1][3]] ;        
-        snake.push(newSnakeSegment);
+        let newSnakeSegment = [snakeArray[snakeArray.length - 1][0] - 20, snakeArray[snakeArray.length - 1][1],
+        snakeArray[snakeArray.length - 1][2], snakeArray[snakeArray.length - 1][3]];
+        snakeArray.push(newSnakeSegment);
     }
 
-    else if (snake[snake.length - 1][0] - snake[snake.length - 1][2] > 0 &&
-         snake[snake.length - 1][3] - snake[snake.length - 1][1] == 0) {
+    else if (snakeArray[snakeArray.length - 1][0] - snakeArray[snakeArray.length - 1][2] > 0 &&
+        snakeArray[snakeArray.length - 1][3] - snakeArray[snakeArray.length - 1][1] == 0) {
 
-        let newSnakeSegment = [snake[snake.length - 1][0] + 20, snake[snake.length-1][1] + 10,
-        snake[snake.length-1][2], snake[snake.length-1][3]] ;
-        snake.push(newSnakeSegment);
+        let newSnakeSegment = [snakeArray[snakeArray.length - 1][0] + 20, snakeArray[snakeArray.length - 1][1] + 10,
+        snakeArray[snakeArray.length - 1][2], snakeArray[snakeArray.length - 1][3]];
+        snakeArray.push(newSnakeSegment);
     }
-    randomAppleCreator();
+
 }
 
 function positionFormatter() {
-    
-    if (snake[0][2] > 1200) {
-        snake[0][2] = snake[0][2] % 1200;
-        snake[0][0] = snake[0][0] % 1200;
-        
+
+    let firstX = snakeArray[0][0];
+    let firstY = snakeArray[0][1];
+    let lastX = snakeArray[0][2];
+    let lastY = snakeArray[0][3]
+
+    if (snakeArray[0][2] > canvasWidth) {
+        lastX = 10;
+        firstX = 0;
+
     }
 
-    else if (snake[0][2] < 0) {
-        snake[0][2] += 1200;
-        snake[0][0] += 1200;
-        
+    else if (snakeArray[0][2] < 0) {
+        lastX = canvasWidth - 10;
+        firstX = canvasWidth;
+
     }
 
-    else if (snake[0][3] > 300) {
-        snake[0][3] = snake[0][3] % 300;
-        snake[0][1] = snake[0][1] % 300;
-        
+    else if (snakeArray[0][3] > canvasHeight) {
+        lastY = 10;
+        firstY = 0;
+
     }
 
-    else if (snake[0][3] < 0) {
-        snake[0][3] += 300;
-        snake[0][1] += 300;
-        
+    else if (snakeArray[0][3] < 0) {
+        lastY = canvasHeight - 10;
+        firstY = canvasHeight;
+
     }
-    newSegment(snake[0][0], snake[0][1], snake[0][2], snake[0][3]);
+    snakeArray.pop();
+    newSegment(firstX, firstY, lastX, lastY);
 }
 
-canvasPosition();
-randomAppleCreator();
+window.onload = () => {
 
+    canvasPosition();
+    randomAppleCreator();
+}
+
+function isGameFinished() {
+
+    snakeHeadX = snakeArray[0][2];
+    snakeHeadY = snakeArray[0][3];
+
+    if (snakeLocation(snakeHeadX, snakeHeadY)) {
+        clearInterval(timer);
+        directionNumber = 0;
+        
+    }
+}
+
+function snakeLocation(locationX, locationY) {
+
+    for (let i = 0; i < snakeArray.length; i++) {
+        if (snakeArray[i][0] === locationX && snakeArray[i][1] === locationY) {
+            return true;
+        }
+    }
+    return false;
+}
 
